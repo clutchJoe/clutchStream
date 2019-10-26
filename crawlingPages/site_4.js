a122e3f511595e41b681ea2f4313882c97c438e3
clutchJoe <maomao126my@126.com>
2019-10-26 17:29:36 +0800

    fix bug

--- crawlingPages/site_4.js
+++ crawlingPages/site_4.js
@@ -50,7 +50,7 @@ module.exports = async (page) => {
                     "body script",
                     els =>
                         els.filter(i => i.textContent.trim().startsWith("var playerElement"))[0].innerText
-                            .split("source: '")[1]
+                            .split("source:'")[1]
                             .split("'")[0]
                 );
                 if(sourceLink == ""){
