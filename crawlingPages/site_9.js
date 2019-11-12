4193e2903d2c9eba99255e737aad33b28a4a9a99
clutchJoe <maomao126my@126.com>
2019-11-12 22:58:38 +0800

    change the site

--- crawlingPages/site_9.js
+++ crawlingPages/site_9.js
@@ -5,25 +5,20 @@ module.exports = async (page) => {
     // const bowser = await puppeteer.launch({ headless: false }); // { headless: false },{ args: ['--no-sandbox'] }
     // const page = await bowser.newPage();
     await page.goto(process.env.SITE_9, { waitUntil: "networkidle2" });
+
     const data = await page.evaluate(() => {
         let lists = [];
-        // const temp = Array.from(document.querySelectorAll("div.page-content p")[1].children);
-        const h1 = Array.from(document.querySelectorAll("div.page-content h1"));
-        let day = '';
-        new Date().getDate() < 10 ? day = '0' + new Date().getDate() : day = new Date().getDate();
-        const tar = h1.find(i => i.innerText.split(" ")[1] == day);
-        const temp = Array.from(tar.nextElementSibling.nextElementSibling.children);
-        const items = temp.filter(i => i.nodeName == "SPAN").filter(i => i.innerText.indexOf("NBA") != -1);
+        const items = Array.from(document.querySelector("div.rapidwp-posts-container").children);
+        const time = document.querySelector("div.rapidwp-header-date").innerText.trim();
         // 判断是否为空数组
         if(items == false){
             return items;
         } else {
-            const time = document.querySelectorAll("div.page-content h1")[2].innerText.split("day ")[1];
             for (let item of items) {
                 let data = {};
-                data.head = item.nextSibling.textContent.trim();
+                data.head = item.children[0].children[0].innerText.trim();
                 data.updateTime = time;
-                data.link = item.nextElementSibling.href;
+                data.link = item.children[0].children[0].children[0].href;
                 lists.push(data);
             }
             return lists;
@@ -32,29 +27,18 @@ module.exports = async (page) => {
 
     if(!(data == false)){
         for (let item of data) {
-            let phpLink = "";
-            try {
-                await page.goto(item.link, { waitUntil: "networkidle2" });
-                await page.frames();
-                phpLink = await page.$$eval("iframe", iframes => iframes.filter(iframe => iframe.src.endsWith(".php"))[0].src);
-            } catch (err) {
-                console.error("site_9: No php link...");
-                item.head = "(No Signal) "  + item.head;
-                item.link = "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8";
-                continue;
-            }
             let sourceLink = "";
             try {
-                await page.goto(phpLink, { waitUntil: "networkidle2" });
+                await page.goto(item.link, { waitUntil: "networkidle2" });
                 sourceLink = await page.$$eval(
                     "body script",
                     els =>
-                        els.find(i => i.innerText.trim().startsWith("var playerElement")).innerText
-                            .split("atob('")[1]
-                            .split("')")[0]
+                        els.find(i => i.innerText.trim().startsWith("var currentTime")).innerText
+                            .split("var hanturl='")[1]
+                            .split("';")[0]
                 );
             } catch (err) {
-                console.error("site_9: something wrong on sourceLink...");
+                console.error("something wrong...");
                 item.head = "(Wrong) "  + item.head;
                 item.link = "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8";
                 continue;
@@ -64,7 +48,7 @@ module.exports = async (page) => {
                 item.head = "(No Signal) "  + item.head;
                 item.link = "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8";
             }else{
-                item.link = Buffer.from(sourceLink, 'base64').toString();
+                item.link = sourceLink;
             }
         }
     }
