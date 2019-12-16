4eb1182384d5d1d815fd95e2012fc3b6b0891535
clutchJoe <maomao126my@126.com>
2019-12-16 23:48:26 +0800

    change the site

--- crawlingPages/site_8.js
+++ crawlingPages/site_8.js
@@ -2,54 +2,38 @@ require("dotenv").config({ path: "../.env" });
 // const puppeteer = require("puppeteer");
 
 module.exports = async (page) => {
-    // const bowser = await puppeteer.launch({ headless: false }); // { headless: false },{ args: ['--no-sandbox'] }
+	// const bowser = await puppeteer.launch({ headless: false }); // { headless: false },{ args: ['--no-sandbox'] }
     // const page = await bowser.newPage();
     await page.goto(process.env.SITE_8, { waitUntil: "networkidle2" });
+
     const data = await page.evaluate(() => {
-        let lists = [];
-        const items = Array.from(document.querySelectorAll("div.evsk"));
-        // 判断是否为空数组
-        if(items == false){
+    	let lists = [];
+    	const items = Array.from(document.querySelectorAll(".entry-header-wrapper"));
+    	if(items == false){
             return items;
         } else {
-            for (let item of items) {
-                let data = {};
-                data.head = item.children[1].children[1].children[0].children[0].children[0].innerText.trim();
-                data.updateTime = item.children[1].children[1].children[0].children[0].children[1].innerText.split('  ')[0].trim().split("\n\n").join('');
-                data.link = item.children[0].href;
+        	for(let item of items){
+        		let data = {};
+                data.head = item.children[1].children[0].children[0].textContent;
+                data.updateTime = item.children[0].children[1].children[1].children[0].textContent;
+                data.link = item.children[1].children[0].children[0].href;
                 lists.push(data);
-            }
-            return lists;
+        	}
+        	return lists;
         }
     });
 
-    if(!(data == false)){
-        for (let item of data) {
-            let temp = "";
+    if(!(data == false)) {
+    	for (let item of data) {
+    		let sourceLink = "";
             try {
                 await page.goto(item.link, { waitUntil: "networkidle2" });
-                temp = await page.$$eval(
-                    "table#dataTable tbody", 
-                    els => 
-                    Array.from(els[1].children)
-                    .find(i => i.children[1].innerText.indexOf("givemeredditstream") != -1)
-                    .children[3].children[0].href
-                );
-            } catch (err) {
-                console.error("site_8: No temp link...");
-                item.head = "(No Signal) "  + item.head;
-                item.link = "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8";
-                continue;
-            }
-            let sourceLink = "";
-            try {
-                await page.goto(temp, { waitUntil: "networkidle2" });
                 sourceLink = await page.$$eval(
                     "body script",
                     els =>
-                        els.find(i => i.innerText.indexOf("Clappr.Player") != -1).innerText
-                            .split("source:")[1]
-                            .split("'")[1]
+                        els.find(i => i.innerText.trim().startsWith("var player")).innerText
+                            .split("url_play = '")[1]
+                            .split("';")[0]
                 );
             } catch (err) {
                 console.error("site_8: something wrong on sourceLink...");
@@ -57,14 +41,14 @@ module.exports = async (page) => {
                 item.link = "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8";
                 continue;
             }
-            
+
             if(sourceLink == ""){
                 item.head = "(No Signal) "  + item.head;
                 item.link = "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8";
             }else{
                 item.link = sourceLink;
             }
-        }
+    	}
     }
 
     // console.log(data);
